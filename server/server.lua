3b69c5e514cfbe48e56db864831d19dd7a1b864b
Joelelfstrom <joelelfstrom@gmail.com>
2021-02-21 20:49:51 +0100

    Add files via upload
    Fixed heading saving, and only spawn cars that's not in the garage (esx_drp_garage)

--- server/server.lua
+++ server/server.lua
@@ -3,9 +3,9 @@ ESX = nil
 TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end) 
 
 RegisterServerEvent("guille_storevehicle")
-AddEventHandler("guille_storevehicle", function(plate, properties)
+AddEventHandler("guille_storevehicle", function(plate, properties, headings)
     local xPlayer = ESX.GetPlayerFromId(source)
-
+    local h = headings
     local pos = json.encode(xPlayer.getCoords())
     local plate = plate
     local vehprop = json.encode(properties)
@@ -14,6 +14,10 @@ AddEventHandler("guille_storevehicle", function(plate, properties)
 
     MySQL.Async.execute("UPDATE owned_vehicles SET position=@position WHERE plate=@plate", {
         ['@position'] = pos,
+        ['@plate'] = plate,
+    })
+    MySQL.Async.execute("UPDATE owned_vehicles SET heading=@heading WHERE plate=@plate", {
+        ['@heading'] = h,
         ['@plate'] = plate,
     })
     MySQL.Async.execute("UPDATE owned_vehicles SET vehicle=@vehicle WHERE plate=@plate", {
@@ -26,14 +30,14 @@ end)
 ESX.RegisterServerCallback('guille_getvehicles', function(source,cb) 
     local _source = source
     local xPlayer = ESX.GetPlayerFromId(_source)
-    MySQL.Async.fetchAll("SELECT vehicle, position FROM owned_vehicles WHERE owner=@identifier", {
+    MySQL.Async.fetchAll("SELECT vehicle, position, heading FROM owned_vehicles WHERE owner=@identifier AND state=0", {
         ['@identifier'] = xPlayer.getIdentifier()
     }, function(result)
         local vehicles = {}
         if result[1] ~= nil then
             for i = 1, #result, 1 do
                 print("test")
-                table.insert(vehicles, { ["position"] = json.decode(result[i]["position"]), ["vehProps"] = json.decode(result[i]["vehicle"]) })
+                table.insert(vehicles, { ["position"] = json.decode(result[i]["position"]), ["h"] = json.decode(result[i]["heading"]), ["vehProps"] = json.decode(result[i]["vehicle"]) })
             end
         end
         cb(vehicles)
