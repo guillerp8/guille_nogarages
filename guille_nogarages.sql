2754fdf339179f17c59d0a589987f90da906bf9a
Joelelfstrom <joelelfstrom@gmail.com>
2021-02-22 09:43:43 +0100

    Right working fixed files
    Added saving heading, also only spawns vehicles that isn't in the garage (esx_dzp_garage)

--- guille_nogarages.sql
+++ guille_nogarages.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `owned_vehicles` ADD `position` LONGTEXT NOT NULL DEFAULT '{"z":21.72,"x":229.00,"y":7432.00}';
-ALTER TABLE `owned_vehicles` ADD `heading` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT '0';
+ALTER TABLE `owned_vehicles` ADD `heading` LONGTEXT NOT NULL DEFAULT '{"heading":95.00}';
